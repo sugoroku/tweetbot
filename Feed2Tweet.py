af787ab6a3194a0f85fbb4f748de1ee26cf806f3
sugoroku <aktkys3@gmail.com>
2017-03-28 15:49:57 +0900

    Add SunriseTweet and Feed2Tweet

---
+++ Feed2Tweet.py
@@ -0,0 +1,119 @@
+#! /home/pi/.pyenv/shims/python
+#! _*_ coding: utf-8 _*_
+
+import feedparser
+import time
+import datetime
+import MySQLdb
+import twython
+
+urllist = [
+'http://www.zou3.net/php/rss/nikkei2rss.php?head=main',
+'http://rss.asahi.com/rss/asahi/newsheadlines.rdf', 
+'http://rss.rssad.jp/rss/mainichi/flash.rss',
+'https://headlines.yahoo.co.jp/rss/san-dom.xml',
+'http://rss.wor.jp/rss1/yomiuri/latestnews.rdf',
+'http://www.zou3.net/php/rss/chunichi2rss.php?cat=main',
+'http://www.nikkan.co.jp/rss/nksrdf.rdf',
+'http://www.security-next.com/feed',
+'https://the01.jp/feed/',
+'http://feed.rssad.jp/rss/gigazine/rss_2.0',
+'http://rss.rssad.jp/rss/itmatmarkit/rss091.xml',
+'http://itpro.nikkeibp.co.jp/rss/news.rdf',
+'http://www.jpcert.or.jp/rss/jpcert.rdf',
+'http://www.ipa.go.jp/security/rss/alert.rdf',
+'http://feeds.trendmicro.com/TM-Securityblog/',
+'https://feed43.com/0681080852134530.xml'
+]
+
+filterlist = ['情報漏洩', '情報漏えい', '情報流出', '不正アクセス', 'ハッキング', '改ざん', '改竄', '不正な通信', 'サイバー攻撃', '標的型', '個人情報']
+
+CONSUMER_KEY    = ''
+CONSUMER_SECRET = ''
+ACCESS_KEY      = ''
+ACCESS_SECRET   = ''
+tw = twython.Twython(CONSUMER_KEY,CONSUMER_SECRET,ACCESS_KEY,ACCESS_SECRET)
+
+dt = datetime.datetime.now()
+
+for url in urllist:
+  fd = feedparser.parse(url)
+  channelurl = fd.feed.link
+  channelname = fd.feed.title
+
+  for entry in fd.entries:
+    for key in filterlist:
+      if key in entry.title :
+        connector = MySQLdb.connect(
+            user = 'root',
+            passwd = 'dbmaster',
+            host = 'localhost',
+            db = 'rss',
+            charset = 'utf8',
+            use_unicode = True)
+
+        cursor = connector.cursor()
+
+        cursor.execute(
+            "select link from incident where link=%s", (entry.link,))
+
+        if len(cursor.fetchall()) == 0:
+          cursor.execute(
+              "insert into incident (link, title, channel, updated) values (%s, %s, %s, %s)", 
+              (entry.link, entry.title, channelurl, dt))
+
+          connector.commit()
+            
+          tweet = "\"" + entry.title + " - " + channelname + "\" " + entry.link
+          print(tweet)
+          tw.update_status(status=tweet)
+
+        cursor.close
+        connector.close
+
+      break
+
+print("------------------ %s ---" % (dt))
+
+# Security Corporation
+#  JPCERT
+#   http://www.jpcert.or.jp/rss/jpcert.rdf
+#  IPA
+#   http://www.ipa.go.jp/security/rss/alert.rdf
+
+# Security Vender
+#  TrendMicro
+#   http://feeds.trendmicro.com/TM-Securityblog/
+#  CISCO TAROS
+#   https://feed43.com/0681080852134530.xml
+
+# IT media
+#  Security-Next
+#   http://www.security-next.com/feed
+#  The Zero/One
+#   https://the01.jp/feed/ 
+#  @IT market
+#   http://rss.rssad.jp/rss/itmatmarkit/rss091.xml
+#  GIGAZINE
+#   http://feed.rssad.jp/rss/gigazine/rss_2.0
+#  ITpro
+#   http://itpro.nikkeibp.co.jp/rss/news.rdf
+
+# News Paper
+#  Asashi
+#   http://rss.asahi.com/rss/asahi/newsheadlines.rdf
+#   https://headlines.yahoo.co.jp/rss/asahik-dom.xml
+#  Mainichi
+#   http://rss.rssad.jp/rss/mainichi/flash.rss
+#  Chyunichi
+#   http://www.zou3.net/php/rss/chunichi2rss.php?cat=main
+#  Yomiuri
+#   http://rss.wor.jp/rss1/yomiuri/latestnews.rdf
+#  Sankei
+#   https://headlines.yahoo.co.jp/rss/san-dom.xml
+#   http://rss.wor.jp/rss1/sankei/flash.rdf
+#  Nikkei
+#   http://www.zou3.net/php/rss/nikkei2rss.php?head=main
+#  Nikkankougyou
+#   http://www.nikkan.co.jp/rss/nksrdf.rdf
+
---
+++ SunriseTweet.py
@@ -0,0 +1,61 @@
+#! /home/pi/.pyenv/shims/python
+#! _*_ coding: utf-8 _*_
+
+import pywapi
+import pprint
+import twython
+import datetime
+import time
+
+CONSUMER_KEY    = ''
+CONSUMER_SECRET = ''
+ACCESS_KEY      = ''
+ACCESS_SECRET   = ''
+api = twython.Twython(CONSUMER_KEY,CONSUMER_SECRET,ACCESS_KEY,ACCESS_SECRET)
+
+result = pywapi.get_weather_from_weather_com('JAXX0085')
+pp = pprint.PrettyPrinter(indent=4)
+
+sunrise = result['forecasts'][1]['sunrise']
+brief_text = result['forecasts'][1]['day']['brief_text']
+high_temp = result['forecasts'][1]['high']
+chance_precip = result['forecasts'][1]['day']['chance_precip']
+
+if brief_text == 'Sunny' or brief_text == 'Clear' or brief_text == 'M Sunny' or brief_text == 'M Clear' or brief_text == 'Sunny/Wind':
+  weather = "晴れ"
+elif brief_text == 'P Cloudy':
+  weather = "晴れ時々曇り"
+elif brief_text == 'Clear Late':
+  weather = "曇りのち晴れ"
+elif brief_text == 'M Cloudy' or brief_text == 'Cloudy':
+  weather = "曇り"
+elif brief_text == 'Rain' or brief_text == 'Rain Early' or brief_text == 'Showers':
+  weather = "雨"
+elif brief_text == 'PM Showers':
+  weather = "午後ににわか雨"
+else:
+  weather = brief_text
+
+tweet = "おはようございます\nまもなく陽が昇ります\n本日の天気は" + weather +"\n最高気温は" + high_temp + "℃\n降水確率は" + chance_precip + "%\n今日も良い日でありますように\nhttps://weather.com/ja-JP/weather/today/l/JAXX0085:1:JA"
+
+now = datetime.datetime.now()
+
+print("SunriseApp start")
+print(now)
+
+d_hour = int(sunrise[0:1],10) - now.hour
+if d_hour < 0:
+  d_hour += 24
+
+d_minute = int(sunrise[2:4],10) - now.minute
+
+d_sec = d_hour*3600 + d_minute*60
+
+print(d_sec)
+time.sleep(d_sec)
+
+pp.pprint(result)
+
+api.update_status(status=tweet)
+print("SunriseApp end")
+
